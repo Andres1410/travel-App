903053da660c091eabd6e1702a873150876ad3b2
Andres1410 <camachorodriguezandresfelipe@gmail.com>
2023-11-09 11:15:36 -0500

    estilos

--- src/components/Search/SearchNav.jsx
+++ src/components/Search/SearchNav.jsx
@@ -12,11 +12,11 @@ class SearchNav extends React.Component {
     render() {
         return (
             <>
-              <div class="grid md:hidden text-center content-around h-full relative md:flex md:items-center  bg-[url('../public/img/paisaje.jpg')] md:bg-[url('../public/img/paisaje.jpg')] bg-cover h-screen md:h-[90vh]">
+              <div class="grid md:hidden text-center content-around h-full relative md:flex md:items-center  bg-[url('../public/img/paisaje.jpg')] md:bg-[url('../public/img/paisaje.jpg')] lg:bg-[url('../public/img/paisaje.jpg')]  bg-cover h-screen md:h-[90vh]">
                 <form action="">
                     <input class="shadow-lg rounded-full py-4 w-3/4 outline-none text-center" placeholder='dubai' type="search" name="" id="" />
                 </form>
-                <button class="text-red text-lg font-semibold rounded-full shadow-lg bg-white hover:bg-red hover:text-white p-4 w-1/2 mx-auto">
+                <button class="text-red text-lg font-semibold rounded-full shadow-lg bg-white hover:bg-red hover:text-white p-4 w-1/2 mx-auto transition transition-all duration-500 easy-in-out hover:bg-primary hover: text-black transform hover:-translate-y-1 hover:scale-110">
                     Explore More
                 </button>
             </div>
